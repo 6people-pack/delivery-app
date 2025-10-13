731b9c57eee7597aa9d9a4d0c361b33fd2d35a88
wlsql852@naver.com <wlsql852@naver.com>
2025-10-14 00:45:43 +0900

    feat : ai 답변 기록 단건 조회

--- src/main/java/com/sparta/delivery/ai/dto/AiResponseDto.java
+++ src/main/java/com/sparta/delivery/ai/dto/AiResponseDto.java
@@ -1,5 +1,6 @@
 package com.sparta.delivery.ai.dto;
 
+import com.sparta.delivery.ai.domain.Ai;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -8,15 +9,18 @@ import java.time.LocalDateTime;
 public class AiResponseDto {
     private String id;
 
+    private String question;
+
     private String answer;
 
     private LocalDateTime createdAt;
 
 //    private String createdBy;
 
-    public AiResponseDto(String id, String answer, LocalDateTime createdAt) {
-        this.id = id;
-        this.answer = answer;
-        this.createdAt = createdAt;
+    public AiResponseDto(Ai ai) {
+        this.id = ai.getId().toString();
+        this.question = ai.getQuestion();
+        this.answer = ai.getAnswer();
+        this.createdAt = ai.getCreatedAt();
     }
 }
