f2f65c1d1657606dd0a437f16afdf74a6d686eab
wlsql852@naver.com <wlsql852@naver.com>
2025-10-13 19:35:28 +0900

    fix : 사용하지 않는 메서드 모두 삭제2

--- src/main/java/com/sparta/delivery/ai/repository/AiRepository.java
+++ src/main/java/com/sparta/delivery/ai/repository/AiRepository.java
@@ -4,8 +4,6 @@ import com.sparta.delivery.ai.domain.Ai;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 public interface AiRepository extends JpaRepository<Ai, UUID> , QuerydslPredicateExecutor<Ai> {
