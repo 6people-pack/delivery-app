6a279984db110f0877d441e3eb8ed20ac335b4e5
kimsj0970 <53886275+kimsj0970@users.noreply.github.com>
2025-10-14 10:46:52 +0900

    fix: id 타입 uuid로 수정

--- src/main/java/com/sparta/delivery/inquiry/repository/InquiryRepository.java
+++ src/main/java/com/sparta/delivery/inquiry/repository/InquiryRepository.java
@@ -2,6 +2,7 @@ package com.sparta.delivery.inquiry.repository;
 
 import com.sparta.delivery.inquiry.domain.Inquiry;
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,7 +15,7 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 
 
     @Query("select i from Inquiry i where i.id = :inquiryId and i.user.id = :userId")
-    Optional<Inquiry> findByIdAndUser(@Param("inquiryId") Long inquiryId, @Param("userId") Long userId);
+    Optional<Inquiry> findByIdAndUser(@Param("inquiryId") UUID inquiryId, @Param("userId") Long userId);
 
 
     @Query("select i from Inquiry i where i.id < :cursor or :cursor IS NULL ORDER BY i.id DESC")
