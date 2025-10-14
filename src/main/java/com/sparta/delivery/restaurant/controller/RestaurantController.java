bd74ffb31486005e0d2919d01efbc8d0101473c7
RaeHwan Park <prh0108@naver.com>
2025-10-14 17:05:41 +0900

    refactor: 사용 안하는 import 제거

--- src/main/java/com/sparta/delivery/restaurant/controller/RestaurantController.java
+++ src/main/java/com/sparta/delivery/restaurant/controller/RestaurantController.java
@@ -8,13 +8,8 @@ import com.sparta.delivery.restaurant.service.RestaurantService;
 import com.sparta.delivery.security.userdetails.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Role;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
