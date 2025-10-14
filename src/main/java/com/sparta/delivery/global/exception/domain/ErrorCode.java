f335527568456c8810dacc2eb70ff58d3f3a9de0
RaeHwan Park <prh0108@naver.com>
2025-10-14 18:42:07 +0900

    누락 ErrorCode 추가

--- src/main/java/com/sparta/delivery/global/exception/domain/ErrorCode.java
+++ src/main/java/com/sparta/delivery/global/exception/domain/ErrorCode.java
@@ -9,11 +9,12 @@ public enum ErrorCode {
     // 사용 예시이므로 추가로 작성 해주시면 찡긋
 
 
+    // 400
     //// 400
     INVALID_REQUEST_DATA(HttpStatus.BAD_REQUEST, "요청 데이터가 올바르지 않습니다. 입력 데이터를 확인해 주세요."),
     PASSWORD_CONFIRM_NOT_MATCH(HttpStatus.BAD_REQUEST, "새 비밀번호와 재확인 비밀번호가 일치하지 않습니다."),
     INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "해당 카테고리를 찾지 못했습니다." ),
-      //image
+    //image
     IMAGE_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당하는 이미지를 찾을 수 없습니다."),
     IMAGE_SAME_INDEX(HttpStatus.BAD_REQUEST, "같은 인덱스 번호로 변경할 수 없습니다."),
     S3_FOLDER_NO_FILE(HttpStatus.NOT_FOUND, "S3 폴더에 파일이 존재하지 않습니다."),
@@ -22,9 +23,10 @@ public enum ErrorCode {
     ZERO_AMOUNT_PAYMENT_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "0원 결제는 허용되지 않습니다."),
 
 
+
     // 401
     AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "인증이 필요한 요청입니다. 로그인 해주세요."),
-        // 비번 변경 시
+    // 비번 변경 시
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다. 다시 시도 해주세요."),
     ROLE_AUTHORIZATION_REQUIRED(HttpStatus.UNAUTHORIZED, "권한이 필요한 요청입니다."),
 
@@ -32,11 +34,14 @@ public enum ErrorCode {
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
 
     //500
-      //image
+    //image
     FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"파일 업로드에 실패했습니다."),
     FILE_DELETE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제에 실패했습니다."),
 
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+
+    // 403
+    FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다."),
 
 
     // 403 Forbidden
