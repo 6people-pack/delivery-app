168d62fd1d8a5b38aaccac8853459fed4b811d4f
wlsql852@naver.com <wlsql852@naver.com>
2025-10-17 18:46:28 +0900

    fix : ai 재시도 @Retry로 변경

--- src/main/java/com/sparta/delivery/ai/service/AiApiService.java
+++ src/main/java/com/sparta/delivery/ai/service/AiApiService.java
@@ -2,8 +2,8 @@ package com.sparta.delivery.ai.service;
 
 import com.sparta.delivery.ai.dto.AiSimpleResponseDto;
 import com.sparta.delivery.ai.event.AiGenerateFailedEvent;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -16,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 
@@ -28,8 +29,6 @@ public class AiApiService {
     private final AiService aiService;
     private final ApplicationEventPublisher publisher;
 
-    private final int MAX_RETRY = 3;
-
     public AiApiService(RestTemplateBuilder builder, AiService aiService, ApplicationEventPublisher publisher) {
         this.restTemplate = builder.build();
         this.aiService = aiService;
@@ -40,45 +39,9 @@ public class AiApiService {
     private String apikey;
 
 
-    //todo : 응답 안받았을때 대응 필요 - 후에 @retry 사용
     //todo : 다른 dto도 id를 UUID로 바꿈
-    //todo : error message 파싱 개선
+    @Retry(name="ai", fallbackMethod = "recoverGenerateAiAnswer")
     public AiSimpleResponseDto generateAiAnswer(Long userId, String category, UUID categoryId, String question) {
-        AiSimpleResponseDto response = null;
-        try{
-            response =  getAnswerFromGemini(question);
-        }catch (Exception e){
-            int attempt = 0;
-            while(attempt < MAX_RETRY){
-                String errorMessage;
-                try{
-                    attempt++;
-                    errorMessage = e.getMessage().substring(e.getMessage().lastIndexOf("message")+10);
-                    errorMessage = errorMessage.substring(0,errorMessage.indexOf("<EOL>"));
-                    log.warn("AI 응답 실패 및 재시도 - userId : {}, category : {}, categoryId : {}, question : {}, attempt : {}, error: {}", userId, category, categoryId, question, attempt, errorMessage);
-                    response = getAnswerFromGemini(question);
-                    break;
-                }catch (Exception ex){
-                    if(attempt >=MAX_RETRY) {
-                        errorMessage = ex.getMessage().substring(ex.getMessage().lastIndexOf("message")+10);
-                        errorMessage = errorMessage.substring(0,errorMessage.indexOf("<EOL>"));
-                        log.error("AI 재시도 최종 실패 - userId : {}, category : {}, categoryId : {}, question : {}, occurredAt: {}, error: {}", userId, category, categoryId, question, LocalDateTime.now(), errorMessage);
-                        response = new AiSimpleResponseDto("AI 응답이 없습니다. 잠시 후 다시 시도해주세요.");
-                        publisher.publishEvent(new AiGenerateFailedEvent(userId, category, categoryId,  ex.getClass().getSimpleName(), errorMessage));
-                    }
-                    try {
-                        Thread.sleep(2000);  //재시도 간격 2초
-                    }catch (InterruptedException exc){
-                        Thread.currentThread().interrupt();
-                    }
-                }
-            }
-        }
-        return response;
-    }
-
-    @NotNull
-    private AiSimpleResponseDto getAnswerFromGemini(String question) {
         //요청 url 만들기
         URI uri = UriComponentsBuilder
                 .fromUriString("https://generativelanguage.googleapis.com")
@@ -97,13 +60,50 @@ public class AiApiService {
         //요청 보내기
         ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
         log.info("AI API Status Code : " + responseEntity.getStatusCode());
-        log.info("AI API Question : " + question);
         String answer = fromJSONtoAnswer(responseEntity.getBody());
-        log.info("AI API Answer : " + answer);
+        DateTimeFormatter timeformat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        log.warn("AI 요청 - userId : {}, category : {}, categoryId : {}, question : {}, answer : {}, timestamp: {}", userId, category, categoryId, question,answer, LocalDateTime.now().format(timeformat));
 
         aiService.saveAi(question, answer);
         return new AiSimpleResponseDto(answer);
     }
+
+    //최종 실패시 discord 알림 및 로그 남기기
+    public AiSimpleResponseDto recoverGenerateAiAnswer(Long userId, String category, UUID categoryId, String question, Throwable e) {
+        String errorMessage = e.getMessage().substring(e.getMessage().lastIndexOf("message")+10);
+        errorMessage = errorMessage.substring(0,errorMessage.indexOf("<EOL>"));
+        log.error("AI 재시도 최종 실패 - userId : {}, category : {}, categoryId : {}, question : {}, occurredAt: {}, error: {}", userId, category, categoryId, question, LocalDateTime.now(), errorMessage);
+        publisher.publishEvent(new AiGenerateFailedEvent(userId, category, categoryId,  e.getClass().getSimpleName(), errorMessage));
+        return new AiSimpleResponseDto("AI 응답이 없습니다. 잠시 후 다시 시도해주세요.");
+    }
+
+//    @NotNull
+//    private AiSimpleResponseDto getAnswerFromGemini(String question) {
+//        //요청 url 만들기
+//        URI uri = UriComponentsBuilder
+//                .fromUriString("https://generativelanguage.googleapis.com")
+//                .path("/v1beta/models/gemini-2.5-flash:generateContent")
+//                .encode()
+//                .build()
+//                .toUri();
+//
+//        //요청 만들기
+//        RequestEntity<String> requestEntity = RequestEntity
+//                .post(uri)
+//                .header("x-goog-api-key",apikey)
+//                .header("Content-Type","application/json")
+//                .body(fromQuestiontoJSON(question));
+//
+//        //요청 보내기
+//        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+//        log.info("AI API Status Code : " + responseEntity.getStatusCode());
+//        log.info("AI 요청 - userId : {}, category : {}, categoryId : {}, question : {}", userId, category, categoryId, question);
+//        String answer = fromJSONtoAnswer(responseEntity.getBody());
+//        log.warn("AI API Answer : " + answer);
+//
+//        aiService.saveAi(question, answer);
+//        return new AiSimpleResponseDto(answer);
+//    }
 
     // 질문을 json 형태로 변환
     private String fromQuestiontoJSON(String question) {
