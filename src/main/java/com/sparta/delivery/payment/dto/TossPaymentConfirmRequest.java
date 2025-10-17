ed066e956dc89ac5cd8ad2a5a36bd2ae8a2e9ec7
gyoseok <dhkk65@gmail.com>
2025-10-17 17:28:13 +0900

    ref: Order, Payment 연동 가능하도록 리팩토링
    -toss.html로 결재 테스트 가능하도록 일부 변경

--- src/main/java/com/sparta/delivery/payment/dto/TossPaymentConfirmRequest.java
+++ src/main/java/com/sparta/delivery/payment/dto/TossPaymentConfirmRequest.java
@@ -3,7 +3,10 @@ package com.sparta.delivery.payment.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.UUID;
+
 public record TossPaymentConfirmRequest(
+        // 정상
 	@NotBlank(message = "결제 키(paymentKey)는 필수 입력값입니다.") String paymentKey,
 
 	@NotBlank(message = "주문 번호(orderId)는 필수 입력값입니다.") String orderId,
